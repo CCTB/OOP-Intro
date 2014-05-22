7e428ec4e8f8a062414f7a4b0bc9c3fd1b21bacd
Dan Gilleland <dgilleland@nait.ca>
2014-05-22 11:06:18 -0600

    OOP-styled Hello World in C#

---
+++ Hello World/HelloOOP/Greeter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloOOP
+{
+    public class Greeter
+    {
+        #region Fields and Properties
+        // traditional Field + Property combination
+        private string _GreetingMessage;
+        public string GreetingMessage
+        {
+            get
+            {
+                return _GreetingMessage;
+            }
+            private set
+            {
+                _GreetingMessage = value;
+            }
+        }
+
+        // Auto-implemented Property (field as a "backing store" is "auto-generated")
+        public string FarewellMessage { get; private set; }
+        #endregion
+
+        #region Constructors
+        public Greeter(string greeting, string farewell)
+        {
+            GreetingMessage = greeting;
+            FarewellMessage = farewell;
+        }
+        #endregion
+
+        #region Methods
+        public string SayGreeting(string name)
+        {
+            return name + ", " + GreetingMessage;
+        }
+
+        public string SayGreeting()
+        {
+            return GreetingMessage;
+        }
+
+        public string SayGoodbye(string name)
+        {
+            return name + " - " + FarewellMessage;
+        }
+
+        public string SayGoodbye()
+        {
+            return FarewellMessage;
+        }
+        #endregion
+    }
+}
---
+++ Hello World/HelloOOP/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare two variables (object references)
+            Greeter walmartEmployee, klingonWarrior;
+
+            // Instantiate (create) the objects
+            walmartEmployee = new Greeter("Welcome to Walmart!",
+                                          "Thank you for shopping at Walmart!!");
+            klingonWarrior = new Greeter("nuqneH! yI'el!",
+                                         "Qapla'!");
+
+            // Use the objects
+            Console.WriteLine("The Walmart Employee sample:");
+            Speak(walmartEmployee, "Dan");
+
+            Console.WriteLine("The Klingon Warrior sample:");
+            Speak(klingonWarrior, "Worf");
+        }
+
+        static void Speak(Greeter someone, string name)
+        {
+            Console.WriteLine(someone.SayGreeting(name));
+            Console.WriteLine(someone.SayGoodbye());
+            Console.WriteLine(); // blank line
+        }
+    }
+}
